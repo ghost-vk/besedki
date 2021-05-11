8f11197416a17efee40be58a720f11c642e6e076
ghost <ghost.vkv@gmail.com>
2021-05-11 16:46:50 +0300

    Add versions to enqueue file

--- functions/enqueue.php
+++ functions/enqueue.php
@@ -15,19 +15,19 @@ function add_jquery() {
 add_action('wp_enqueue_scripts', 'add_styles');
 function add_styles() {
 	// General styles
-	wp_enqueue_style( 'main', get_template_directory_uri() . '/style/main.css' );
+	wp_enqueue_style( 'main', get_template_directory_uri() . '/style/main.css?v=1.0' );
 	wp_enqueue_style( 'datetimepicker', get_template_directory_uri() . '/libraries/datetimepicker/jquery.datetimepicker.css' );
 	
 	// Homepage
 	if ( is_front_page() ) { // Homepage
 		wp_enqueue_style( 'slick', get_template_directory_uri() . '/libraries/slick/slick.css' );
-		wp_enqueue_style( 'homepage', get_template_directory_uri() . '/style/homepage.css' );
+		wp_enqueue_style( 'homepage', get_template_directory_uri() . '/style/homepage.css?v=1.0' );
 		wp_enqueue_style( 'animate', get_template_directory_uri() . '/node_modules/animate.css/animate.min.css' );
 	}
 	
 	// Reviews
 	if ( is_page('reviews') ) { // Reviews
-		wp_enqueue_style( 'reviews', get_template_directory_uri() . '/style/reviews.css' );
+		wp_enqueue_style( 'reviews', get_template_directory_uri() . '/style/reviews.css?v=1.0' );
 	}
 	
 	// Document pages (privacy, agreements)
@@ -38,7 +38,7 @@ function add_styles() {
 			'rent-agreement', // Оферта
 		]
 	) ) {
-		wp_enqueue_style( 'page-document', get_template_directory_uri() . '/style/page-document.css' );
+		wp_enqueue_style( 'page-document', get_template_directory_uri() . '/style/page-document.css?v=1.0' );
 	}
 	
 	// Cart + checkout
@@ -48,13 +48,13 @@ function add_styles() {
 			'checkout',
 		)
 	) ) {
-		wp_enqueue_style( 'page-document', get_template_directory_uri() . '/style/cart.css' );
+		wp_enqueue_style( 'page-document', get_template_directory_uri() . '/style/cart.css?v=1.0' );
 	}
 	
 	// Shop page - Reservation page
 	if ( is_archive() ) {
 		wp_enqueue_style( 'custom-select-style', get_template_directory_uri() . '/libraries/custom-select-box/select.css' );
-		wp_enqueue_style( 'reservation', get_template_directory_uri() . '/style/reservation-page/booking.css' );
+		wp_enqueue_style( 'reservation', get_template_directory_uri() . '/style/reservation-page/booking.css?v=1.0' );
 	}
 }
 
@@ -69,16 +69,16 @@ function add_scripts () {
 	 * General scripts
 	 */
 	// Utils library
-	wp_enqueue_script('utils-besedka', $directory . '/js/lib/utils.js', array(), null, true);
+	wp_enqueue_script('utils-besedka', $directory . '/js/lib/utils.js?v=1.0', array(), null, true);
 	
 	// State management
-	wp_enqueue_script('store', $directory . '/js/lib/store.js', array('utils-besedka'), null, true);
+	wp_enqueue_script('store', $directory . '/js/lib/store.js?v=1.0', array('utils-besedka'), null, true);
 	
 	// Notification class
-	wp_enqueue_script('notification', $directory . '/js/lib/Notification.js', array('jquery'), null, true);
+	wp_enqueue_script('notification', $directory . '/js/lib/Notification.js?v=1.0', array('jquery'), null, true);
 	
 	// Notify about cookies
-	wp_enqueue_script('cookies-notification', $directory . '/js/cookies-notification.js', array('js-cookie', 'notification'), null, true);
+	wp_enqueue_script('cookies-notification', $directory . '/js/cookies-notification.js?v=1.0', array('js-cookie', 'notification'), null, true);
 	
 	// https://fontawesome.com/
 	wp_enqueue_script('font-awesome', 'https://kit.fontawesome.com/519fd0f28a.js', array(), null, true);
@@ -90,25 +90,25 @@ function add_scripts () {
 //	wp_enqueue_script('uncompleted-message', $directory . '/js/show-uncomplete.js', array('notification', 'js-cookie'), null, true);
 	
 	// Works with header
-	wp_enqueue_script('header', $directory . '/js/header.js', array('jquery'), null, true);
+	wp_enqueue_script('header', $directory . '/js/header.js?v=1.0', array('jquery'), null, true);
 	
 	// ServerClient class
-	wp_enqueue_script('server-client', $directory . '/js/lib/ServerClient.js', array('jquery'), null, true);
+	wp_enqueue_script('server-client', $directory . '/js/lib/ServerClient.js?v=1.0', array('jquery'), null, true);
 	
 	// Check phone library
 	wp_enqueue_script('libphonenumber', 'https://unpkg.com/libphonenumber-js@1.x/bundle/libphonenumber-min.js', array(), null, true);
 
 	// Callback function
-	wp_enqueue_script('callback', $directory . '/js/callback.js', array('jquery', 'libphonenumber', 'notification'), null, true);
+	wp_enqueue_script('callback', $directory . '/js/callback.js?v=1.0', array('jquery', 'libphonenumber', 'notification'), null, true);
 	
 	// Localize callback
 	wp_localize_script('store', 'callbackText', array( 'text' => get_field('notification_callback_success', 18) )); // 18 - home page ID
 	
 	// Loader class
-	wp_enqueue_script('loader', $directory . '/js/lib/Loader.js', array('jquery'), null, true);
+	wp_enqueue_script('loader', $directory . '/js/lib/Loader.js?v=1.0', array('jquery'), null, true);
 	
 	// Show loader on page loading
-	wp_enqueue_script('page-loader', $directory . '/js/page-loader.js', array('loader'), null, true);
+	wp_enqueue_script('page-loader', $directory . '/js/page-loader.js?v=1.0', array('loader'), null, true);
 	
 	// Localize store with primary settings
 	wp_localize_script('store', 'mainSettings', array(
@@ -131,7 +131,7 @@ function add_scripts () {
 		// BOOKING POPUP
 		
 		// Main handler
-		wp_enqueue_script('popup-lib', $directory . '/js/lib/popup/Popup.js', array(
+		wp_enqueue_script('popup-lib', $directory . '/js/lib/popup/Popup.js?v=1.0', array(
 			'store',
 			'slider-handler',
 			'select-handler',
@@ -141,22 +141,22 @@ function add_scripts () {
 		), null, true);
 		
 		// Slider in PopUp
-		wp_enqueue_script('slider-handler', $directory . '/js/lib/popup/Slider.js', array('jquery', 'slick'), null, true);
+		wp_enqueue_script('slider-handler', $directory . '/js/lib/popup/Slider.js?v=1.0', array('jquery', 'slick'), null, true);
 		
 		// Selector in PopUp
-		wp_enqueue_script('select-handler', $directory . '/js/lib/popup/SelectDuration.js', array('jquery', 'custom-select'), null, true);
+		wp_enqueue_script('select-handler', $directory . '/js/lib/popup/SelectDuration.js?v=1.0', array('jquery', 'custom-select'), null, true);
 		
 		// Datetimepicker in PopUp handler
-		wp_enqueue_script('datetime-handler', $directory . '/js/lib/popup/DatetimeHandler.js', array('jquery', 'datetimepicker'), null, true);
+		wp_enqueue_script('datetime-handler', $directory . '/js/lib/popup/DatetimeHandler.js?v=1.0', array('jquery', 'datetimepicker'), null, true);
 		
 		// Server request in PopUp
-		wp_enqueue_script('booking', $directory . '/js/lib/popup/Booking.js', array(), null, true);
+		wp_enqueue_script('booking', $directory . '/js/lib/popup/Booking.js?v=1.0', array(), null, true);
 		
 		
 		// MAP
 		
 		// Main handler
-		wp_enqueue_script('map-handler', $directory . '/js/lib/map/MapHandler.js', array(
+		wp_enqueue_script('map-handler', $directory . '/js/lib/map/MapHandler.js?v=1.0', array(
 			'jquery',
 			'map-popup',
 			'map-filter',
@@ -164,10 +164,10 @@ function add_scripts () {
 		), null, true);
 		
 		// Works with PopUp on map
-		wp_enqueue_script('map-popup', $directory . '/js/lib/map/MapPopup.js', array('jquery'), null, true);
+		wp_enqueue_script('map-popup', $directory . '/js/lib/map/MapPopup.js?v=1.0', array('jquery'), null, true);
 
 		// Filter points on map
-		wp_enqueue_script('map-filter', $directory . '/js/lib/map/MapFilter.js', array('jquery'), null, true);
+		wp_enqueue_script('map-filter', $directory . '/js/lib/map/MapFilter.js?v=1.0', array('jquery'), null, true);
 		
 		// Localize store to works with map
 		wp_localize_script('store', 'mapState', array(
@@ -177,7 +177,7 @@ function add_scripts () {
 		));
 		
 		// Start reservation page scripts
-		wp_enqueue_script('reservation', $directory . '/js/reservation.js', array('map-handler', 'popup-lib'), null, true);
+		wp_enqueue_script('reservation', $directory . '/js/reservation.js?v=1.0', array('map-handler', 'popup-lib'), null, true);
 		
 	}
 	
@@ -193,10 +193,10 @@ function add_scripts () {
 		wp_enqueue_script('viewport-checker', $directory . '/libraries/viewportchecker/viewportChecker.js', array('jquery'), null, true);
 		
 		// Stick reservation button
-		wp_enqueue_script('sticky-button', $directory . '/js/sticky-button.js', array('jquery'), null, true);
+		wp_enqueue_script('sticky-button', $directory . '/js/sticky-button.js?v=1.0', array('jquery'), null, true);
 		
 		// Starts homepage scripts
-		wp_enqueue_script('homepage', $directory . '/js/homepage.js', array('jquery', 'slick', 'viewport-checker'), null, true);
+		wp_enqueue_script('homepage', $directory . '/js/homepage.js?v=1.0', array('jquery', 'slick', 'viewport-checker'), null, true);
 	}
 	
 	/**
@@ -205,7 +205,7 @@ function add_scripts () {
 	if ( is_page('reviews') ) {
 		
 		// Stick reservation button
-		wp_enqueue_script('sticky-button', $directory . '/js/sticky-button.js', array('jquery'), null, true);
+		wp_enqueue_script('sticky-button', $directory . '/js/sticky-button.js?v=1.0', array('jquery'), null, true);
 	}
 	
 	/**
@@ -231,6 +231,6 @@ function add_scripts () {
 		wp_enqueue_script('mask-input', $directory . '/libraries/jquery.maskedinput/src/jquery.maskedinput.js', array('jquery'), null, true);
 		
 		// Validate checkout fields
-		wp_enqueue_script('checkout-validation', $directory . '/js/checkout-validation.js', array('jquery', 'mask-input'), null, true);
+		wp_enqueue_script('checkout-validation', $directory . '/js/checkout-validation.js?v=1.0', array('jquery', 'mask-input'), null, true);
 	}
 }
