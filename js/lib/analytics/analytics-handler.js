fb0261001bb93b7582647a710f46f4103ced0de6
ghost <ghost.vkv@gmail.com>
2021-09-21 12:10:16 +0300

    Fix: promise error in loading analytics

--- js/lib/analytics/analytics-handler.js
+++ js/lib/analytics/analytics-handler.js
@@ -1,11 +1,10 @@
 import { loadAnalytics } from "./analytics-source";
 import analytics from "./analytics-data";
 
-export const startAnalyticsAfterLoading = async () => {
-    try {
-        await loadAnalytics()
-        analytics.startAnalytics()
-    } catch (err) {
-        console.log('Failed when connecting to analytics systems ...')
-    }
+export const startAnalyticsAfterLoading = () => {
+    loadAnalytics()
+        .then(analytics.startAnalytics)
+        .catch((err) => {
+            console.log('Failed when connecting to analytics systems ...')
+        })
 }
