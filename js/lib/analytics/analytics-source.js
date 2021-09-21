216a57db3edaca560e87f79e40338ab095a35209
ghost <ghost.vkv@gmail.com>
2021-09-21 14:56:06 +0300

    Fix: v4

--- js/lib/analytics/analytics-source.js
+++ js/lib/analytics/analytics-source.js
@@ -36,14 +36,14 @@ const loadGoogle = () => {
         const src = 'https://www.googletagmanager.com/gtag/js?id=UA-196988125-1'
         utils.loadSource(src, true)
             .then(() => {
-                if (typeof gtag === "undefined") {
+                if (typeof window['gtag'] === "undefined") {
                     console.log('Google analytics is not loaded');
                     return false;
                 }
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'UA-196988125-1');
+                window.dataLayer = window.dataLayer || []
+                window['gtag'] = () => { dataLayer.push(arguments) }
+                window['gtag']('js', new Date())
+                window['gtag']('config', 'UA-196988125-1')
                 resolve()
             })
             .catch(() => {
